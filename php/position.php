15f91f219b41283d402f3953bbbb065e8052232e
Conny Ljunggren <conny.ljunggren@ha.ax>
2018-03-22 23:10:28 +0200

    added missing position class & added tests for moving a piece 1 step sideways

---
+++ php/position.php
@@ -0,0 +1,38 @@
+<?php
+class Position
+{
+    private $row;
+    private $col;
+    private const MIN_VALUE = 0;
+    private const MAX_VALUE = 7;
+    
+    public function __construct($row, $col)
+    {
+/*         $this->validateValue($row);
+        $this->validateValue($col); */
+        $this->row = $row;
+        $this->col = $col;
+    }
+    
+    public function getDelta($position)
+    {
+        return new Position($this->row - $position->getRow(), $this->col - $position->getCol());
+    }
+    
+    public function getRow()
+    {
+        return $this->row;
+    }
+    
+    public function getCol()
+    {
+        return $this->col;
+    }
+    
+    private function validateValue($val)
+    {
+        if ($val < self::MIN_VALUE || $val > self::MAX_VALUE) {
+            throw new Exception("Failed to validate position value: " . $val . ".\n");
+        }
+    }
+}
--- php/test/moverule.php
+++ php/test/moverule.php
@@ -13,6 +13,15 @@ assert(!$oneStepForward->validateMove(new Position(1, 1), new Position(1, 1)));
 assert(!$oneStepForward->validateMove(new Position(1, 1), new Position(1, 2)));
 assert(!$oneStepForward->validateMove(new Position(1, 1), new Position(2, 2)));
 
+$oneStepRight = new MoveRule(0, 1, false);
+assert($oneStepRight->validateMove(new Position(1, 1), new Position(1, 2)));
+assert($oneStepRight->validateMove(new Position(2, 2), new Position(2, 3)));
+assert(!$oneStepRight->validateMove(new Position(1, 1), new Position(1, 3)));
+assert(!$oneStepRight->validateMove(new Position(1, 1), new Position(1, 0)));
+assert(!$oneStepRight->validateMove(new Position(1, 1), new Position(1, 1)));
+assert(!$oneStepRight->validateMove(new Position(1, 1), new Position(2, 1)));
+assert(!$oneStepRight->validateMove(new Position(1, 1), new Position(2, 2)));
+
 /* $anyStepForward = new MoveRule(1, 0, true);
 assert($anyStepForward->validateMove(new Position(1, 1), new Position(2, 1)));
 assert($anyStepForward->validateMove(new Position(2, 2), new Position(3, 2)));
