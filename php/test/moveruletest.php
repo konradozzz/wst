22b2edb081011aadf247739a8b1ca846c521cbc0
Conny Ljunggren <conny.ljunggren@ha.ax>
2020-03-09 00:21:11 +0200

    some tests to verify MoveRule

--- php/test/moveruletest.php
+++ php/test/moveruletest.php
@@ -3,21 +3,48 @@ require("../Position.php");
 require("../BoardState.php");
 require("../MoveRule.php");
 
-$movePath = array(  new Position(7, 1),
+function compareArrays($a1, $a2)
+{
+    sort($a1);
+    sort($a2);
+    return $a1 == $a2;
+}
+
+$movePath = array(  new Position(1, 1),
                     new Position(2, 1),
                     new Position(3, 1),
                     new Position(4, 1),
                     new Position(5, 1),
                     new Position(6, 1));
-                    
+
 $positions = array( new Position(0, 1),
-                    new Position(0, 1),
+                    new Position(4, 1),
                     new Position(2, 0));
                     
 $boardState = new BoardState($positions);
 
-$moveRule = new MoveRule(true, true);
 
+$check = array( new Position(1, 1),
+    new Position(2, 1),
+    new Position(3, 1),
+    new Position(4, 1));
+
+$moveRule = new MoveRule(true, false);
 $validatedMoves = $moveRule->validateMoves($movePath, $boardState);
+assert(compareArrays($check, $validatedMoves));
 
-print_r($validatedMoves);
+
+$check = array( new Position(1, 1),
+    new Position(2, 1),
+    new Position(3, 1));
+
+$moveRule = new MoveRule(false, false);
+$validatedMoves = $moveRule->validateMoves($movePath, $boardState);
+assert(compareArrays($check, $validatedMoves));
+
+
+
+$moveRule = new MoveRule(false, true);
+$validatedMoves = $moveRule->validateMoves($movePath, $boardState);
+assert(compareArrays(array(), $validatedMoves));
+//print_r($validatedMoves);
